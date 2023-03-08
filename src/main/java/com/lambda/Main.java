cb3932ccf9f752b017c2a033724293b87ac40a8c
gguerrero666 <guillermo_guerrero@outlook.com>
2023-03-08 15:14:42 -0600

    save data into AWS DynamoDB

--- src/main/java/com/lambda/Main.java
+++ src/main/java/com/lambda/Main.java
@@ -1,5 +1,9 @@
 package com.lambda;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.lambda.util.Util;
@@ -7,7 +11,12 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main implements RequestHandler<Request, Object> {
+
+    private AmazonDynamoDB amazonDynamoDB;
 
     public static void main(String[] args) {
         Main m = new Main();
@@ -15,10 +24,6 @@ public class Main implements RequestHandler<Request, Object> {
                 "https://www.homedepot.com.mx/organizadores-y-closets/closets/closets-armables/closet-playcon-mod-supremo-5-cajones-242-cm-grafito-texturizado-159373",
                 "offerPrice_1074152",
                 "id");
-//        Request request = new Request("GET",
-//                "https://www.walmart.com.mx/juguetes/carritos-y-radiocontrol/aviones-y-helicopteros-de-control-remoto/smartphone-samsung-m23-128gb-verde-desbloqueado_00880609418881",
-//                "product-price",
-//                "id");
         System.out.println(m.handleRequest(request, null));
     }
 
@@ -45,6 +50,11 @@ public class Main implements RequestHandler<Request, Object> {
                             Elements c = htmlCode.select(Util.getChar(request.getTagType()) + request.getTagId());
                             if (c.eachText().size() > 0) {
                                 request.setPrice(c.eachText().get(0).replace("00", ".00"));
+
+                                // DynamoDB Code:
+                                this.initDynamoDbClient();
+                                request.setSaved(saveData(request));
+
                             }
                         }
                         request.setFound(found);
@@ -52,6 +62,7 @@ public class Main implements RequestHandler<Request, Object> {
                     } catch (Exception e) {
                         throw new RuntimeException(e);
                     }
+
                     return request;
                 }
             }
@@ -61,6 +72,29 @@ public class Main implements RequestHandler<Request, Object> {
         }
         return null;
     }
-}
 
-//<span id="offerPrice_1074152" class="price">	 <small>$</small>6,979<sup>00</sup>	 <span></span>	 </span>
+
+    public boolean saveData(Request request) {
+
+        String DYNAMODB_TABLE_NAME = "price";
+
+        Map<String, AttributeValue> attributesMap = new HashMap<>();
+
+        attributesMap.put("httpMethod", new AttributeValue(String.valueOf(request.getHttpMethod())));
+        attributesMap.put("url", new AttributeValue(request.getUrl()));
+        attributesMap.put("tagId", new AttributeValue(request.getTagId()));
+        attributesMap.put("tagType", new AttributeValue(String.valueOf(request.getTagType())));
+        attributesMap.put("price", new AttributeValue(request.getPrice()));
+
+        amazonDynamoDB.putItem(DYNAMODB_TABLE_NAME, attributesMap);
+
+        return true;
+
+    }
+
+    private void initDynamoDbClient() {
+        this.amazonDynamoDB = AmazonDynamoDBClientBuilder.standard()
+                .withRegion(Regions.US_EAST_1)
+                .build();
+    }
+}
--- src/main/java/com/lambda/Request.java
+++ src/main/java/com/lambda/Request.java
@@ -25,4 +25,6 @@ public class Request {
 
     private String price;
 
+    private boolean saved;
+
 }
